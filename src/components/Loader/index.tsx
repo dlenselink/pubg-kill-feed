9d2120810e4eb1549a9399e220d074a4555afaf6
Danny Lenselink <dlenselink@rangempls.com>
2020-04-25 10:49:27 -0500

    Updated header CSS

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,12 +1,14 @@
 import React, { useContext }  from "react";
+import * as classnames from "classnames";
 import { GlobalContext } from "../App";
 
 export const Loader = () => {
   const globalContext = useContext(GlobalContext);
+  const classNames = classnames({"loader": true}, {"is-loading": globalContext.isLoading});
 
   return (
-    <div className={globalContext.isLoading ? "loader-image-wrapper is-loading" : "loader-image-wrapper"} >
-      <img className="loader-image" src={require("./loading.gif")} alt="Loading..." />
+    <div className={classNames} >
+      <img className="image" src={require("./loading.gif")} alt="Loading..." />
     </div>
   );
 };
