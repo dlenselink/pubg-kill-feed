44c4a1445ee0c12d926ff57b71e3bd472ba3afa4
Danny Lenselink <dlenselink@rangempls.com>
2020-04-25 10:16:06 -0500

    Updated CSS I

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,9 +5,21 @@ import $ from "jquery";
 
 export const Header = () => {
   const globalContext = useContext(GlobalContext);
+
+  const isTouchDevice = () => {
+    const touchSupport =
+      "onstart" in window ||
+      navigator.maxTouchPoints > 0 ||
+      navigator.msMaxTouchPoints > 0;
+    if (touchSupport) {
+      return true;
+    } else {
+      return false;
+    }
+  };
   
 	const handleKeyPress = async (event: any) => {
-    const searchbarInput: JQuery<HTMLElement> = $(".searchbar-input");
+    const searchbarInput: JQuery<HTMLElement> = $("input[name='searchbarInput']");
 		if (searchbarInput.val() && searchbarInput.is(":hover") === true && event.key === "Enter") {
       globalContext.setIsLoading(true);
       const playerName = String(searchbarInput.val());
@@ -20,8 +32,8 @@ export const Header = () => {
   return(
     <div className="header">
       <div className="searchbar">
-        <input className="searchbar-input" name="searchbarInput" onKeyPress={handleKeyPress} placeholder="Search..." type="text" />
-        <svg className="bi bi-search searchbar-icon" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <input className="input" name="searchbarInput" onKeyPress={handleKeyPress} placeholder="Search..." type="text" />
+        <svg className="bi bi-search icon" fill="currentColor"  viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z" clipRule="evenodd"/>
           <path fillRule="evenodd" d="M6.5 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zM13 6.5a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z" clipRule="evenodd"/>
         </svg>
