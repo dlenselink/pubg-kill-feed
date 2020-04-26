269442ab216df95b0726a4d8ea107be627dce5c2
Danny Lenselink <dlenselink@rangempls.com>
2020-04-25 20:00:05 -0500

    svg from cdn

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -5,10 +5,11 @@ import { GlobalContext } from "Components/App";
 export const Loader = () => {
   const globalContext = useContext(GlobalContext);
   const classNames = classnames({"loader": true}, {"is-loading": globalContext.isLoading});
+  const loadingAnimation = "https://res.cloudinary.com/pubg-kill-feed/image/upload/v1587862298/assets/loading.svg";
 
   return (
     <div className={classNames} >
-      <img className="image" src={require("Assets/loading.svg")} alt="Loading..." />
+      <img className="image" src={loadingAnimation} alt="Loading..." />
     </div>
   );
 };
